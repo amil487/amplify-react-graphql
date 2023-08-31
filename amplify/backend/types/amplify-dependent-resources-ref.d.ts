c7ba40c85d338243b98e6c074a6ada23a004ae3f
Amil Patel <amilpatel26@yahoo.com>
2023-08-30 20:44:18 -0400

    graphQL api integrate and ui change

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql96cb750a": {
       "AppClientID": "string",
