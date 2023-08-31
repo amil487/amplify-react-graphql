408270c1fc8f47add343f4edf9fdcd9a6118dd2a
Amil Patel <amilpatel26@yahoo.com>
2023-08-30 21:17:16 -0400

    image support added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3997f07df": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
